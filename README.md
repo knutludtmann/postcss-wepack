0069481f52252eb9add25fd70176e9e4fd9396fb
Knut Ludtmann <k.ludtmann@lrworld.com>
2017-05-11 16:55:45 +0200

    source-map added

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
-## webpack 2 + PostCSS + cssnext
+## webpack 2 + PostCSS + cssnext + sourcemaps
 
 
 https://blog.madewithenvy.com/webpack-2-postcss-cssnext-fdcd2fd7d0bd
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,40 @@
 const path = require('path'),
     webpack = require('webpack'),
     ExtractTextPlugin = require('extract-text-webpack-plugin'),
-    BrowserSyncPlugin = require('browser-sync-webpack-plugin'),
-    OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+    BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     entry: {
         app: './app.js'
     },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            exclude: ['/node_modules/'],
+            use: ExtractTextPlugin.extract({
+                /* fallbackLoader: 'style-loader', //importLoaders=1? */
+                loader: [
+                    { loader: 'css-loader', query: { sourceMap: true } },
+                    { loader: 'postcss-loader', query: { sourceMap: true } }
+                ],
+            }),
+        }, ],
+    },
     plugins: [
-        new ExtractTextPlugin('[name].bundle.css'),
         new BrowserSyncPlugin({
             host: 'localhost',
             port: 3006,
             server: { baseDir: ['./'] }
         }),
+        new ExtractTextPlugin('[name].bundle.css'),
     ],
+
+    devtool: 'source-map',
     watch: true,
-    module: {
-        rules: [{
-            test: /\.css$/,
-            use: ExtractTextPlugin.extract({
-                loader: 'css-loader?importLoaders=1!postcss-loader'
-            }),
-        }, ],
-    },
     output: {
         path: path.resolve(__dirname, 'dist/assets'),
-        filename: '[name].bundle.css'
+        filename: '[name].bundle.css',
+        sourceMapFilename: '[file].map'
     }
 }
